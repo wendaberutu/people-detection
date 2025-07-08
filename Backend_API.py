e9ff5db34ba65948bd03b4301ad1b59d86925527
GedeAbdi15 <abdi.wicaksana58@gmail.com>
2025-07-08 11:53:26 +0700

    memngoneksikan ke database

--- Backend_API.py
+++ Backend_API.py
@@ -1,2 +1,38 @@
-from flask import Flask,request, jsonify
-from
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import pymysql
+from pymysql.cursors import DictCursor
+
+app = Flask(__name__)
+CORS(app)
+
+# --- KONFIGURASI DATABASE ---
+db_config = {
+    "host": "127.0.0.1",
+    "port": 3307,
+    "user": "root",
+    "password": "",  # ganti kalau pakai password
+    "database": "cctv_db",
+    "cursorclass": DictCursor
+}
+
+
+def get_db_connection():
+    return pymysql.connect(**db_config)
+
+# --- CEK KONEKSI ---
+@app.route('/')
+def index():
+    try:
+        conn = get_db_connection()
+        with conn.cursor() as cur:
+            cur.execute("SELECT VERSION()")
+            version = cur.fetchone()
+        conn.close()
+        return jsonify({"message": "Connected!", "version": version['VERSION()']})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+# --- ENDPOINT LAIN BISA MENYUSUL ---
+if __name__ == '__main__':
+    app.run(debug=True)
